8b055c2cb1ded7edd49cf61caead7b7a3c5aab05
墨兄 <67450326+Momoooc@users.noreply.github.com>
2026-04-21 19:14:32 +0800

    Update data.js

--- web_conf/data.js
+++ web_conf/data.js
@@ -859,7 +859,7 @@ const bookmarkData = [
   },
   {
     urlText: "星空组网",
-    linkUrl: "http://starvpn.cn/register.html?referralCode=96064876",
+    linkUrl: "https://starvpn.cn/",
     linkTitle: "星空, 星空组网, 星空云官网, sd-wan, 组网, 异地组网, 公网ip, 视频监控, 远程访问",
   },
   {
